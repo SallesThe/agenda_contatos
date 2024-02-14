19be941cd3733571c9f228440adb20c94d48583d
SallesThe <manodada26@gmail.com>
2024-02-14 13:47:48 -0300

    inserting changes on home file

--- Vendor/composer/autoload_real.php
+++ Vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInit6476740c69df9a45acadcc4344b06a8a
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit6476740c69df9a45acadcc4344b06a8a::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
--- command.sql
+++ command.sql
@@ -2,5 +2,6 @@ CREATE TABLE tb_contact(
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(60) UNIQUE,
     email VARCHAR(60),
-    tel VARCHAR(20)
+    tel VARCHAR(20),
+    address VARCHAR(50)
 );
